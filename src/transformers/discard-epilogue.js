5861f77369a7a22406d29a6eaa69fc05e894fb6a
Timothy Wong <tim@ai4.tw>
2026-02-22 15:44:19 +0800

    fix: Early termination

--- src/transformers/discard-epilogue.js
+++ src/transformers/discard-epilogue.js
@@ -9,47 +9,51 @@
  */
 async function* ketu(chunks) {
   let remainingTail, foundTail;
-  let { value: chunk, done } = await chunks.next();
+  try {
+    let { value: chunk, done } = await chunks.next();
 
-  while (!done) {
-    switch (remainingTail) {
-      case 2:
-        if (chunk.length === 1) {
-          if (chunk[0] === 0x2d) {
-            remainingTail = 1;
+    while (!done) {
+      switch (remainingTail) {
+        case 2:
+          if (chunk.length === 1) {
+            if (chunk[0] === 0x2d) {
+              remainingTail = 1;
+            } else {
+              yield chunk;
+              remainingTail = undefined;
+            }
           } else {
-            yield chunk;
-            remainingTail = undefined;
+            if (chunk[0] === 0x2d && chunk[1] === 0x2d) {
+              foundTail = true;
+              remainingTail = 0;
+            } else {
+              yield chunk;
+              remainingTail = undefined;
+            }
           }
-        } else {
-          if (chunk[0] === 0x2d && chunk[1] === 0x2d) {
+          break;
+        case 1:
+          if (chunk[0] === 0x2d) {
             foundTail = true;
             remainingTail = 0;
           } else {
+            yield new Uint8Array([0x2d]);
             yield chunk;
             remainingTail = undefined;
           }
-        }
-        break;
-      case 1:
-        if (chunk[0] === 0x2d) {
-          foundTail = true;
-          remainingTail = 0;
-        } else {
-          yield new Uint8Array([0x2d]);
+          break;
+        case 0:
+          break;
+        default:
+          if (!chunk) {
+            remainingTail = 2;
+          }
           yield chunk;
-          remainingTail = undefined;
-        }
-        break;
-      case 0:
-        break;
-      default:
-        if (!chunk) {
-          remainingTail = 2;
-        }
-        yield chunk;
+      }
+      ({ value: chunk, done } = await chunks.next(foundTail));
     }
-    ({ value: chunk, done } = await chunks.next(foundTail));
+  } finally {
+    await chunks.return?.();
   }
 }
 

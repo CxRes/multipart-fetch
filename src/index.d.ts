03a0a15c2134e82d8db9f2a8c108351d60c3d1d6
Timothy Wong <tim@ai4.tw>
2026-02-23 01:13:38 +0800

    feat: Add type declarations

---
+++ src/index.d.ts
@@ -0,0 +1,18 @@
+/*!
+ *  Copyright (c) 2024, Timothy Wong and Multipart Fetch contributors.
+ *
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ *  SPDX-License-Identifier: MPL-2.0
+ */
+
+export function parseBody(streamish: AsyncIterable<Uint8Array>): Promise<Response>;
+
+export default function multipartFetch(
+  response: Response | { body: AsyncIterable<Uint8Array>; headers?: HeadersInit }
+): Response & AsyncIterable<Response> & {
+  readonly subtype: string;
+  parts(): AsyncGenerator<Response>;
+};
